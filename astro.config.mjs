70bce858f6fc1ce2d300c31e693a6dd1af5028f8
bayonadrian <bayonadrian0313@gmail.com>
2026-07-04 19:47:48 -0400

    configure for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Bayonadrian.github.io',
+  base: '/PSAstro-Website',
   vite: {
     plugins: [tailwindcss()]
   },
