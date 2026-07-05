05206da95a0aaf19bf55838896559b8b705b1362
bayonadrian <bayonadrian0313@gmail.com>
2026-07-04 21:20:46 -0400

    modify astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://thepolysight.com',
-  //base: '/PSAstro-Website',
+
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react()]
+  integrations: [react()],
 });
